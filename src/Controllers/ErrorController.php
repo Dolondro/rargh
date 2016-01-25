98d166ae8879425fc7efe28e3839390b3162d838
Doug <dougnelson@silktide.com>
2016-01-25 21:37:53 +0000

    added better mobile support for 404 page, everyoem deserves some lionel in their lives

--- src/Controllers/ErrorController.php
+++ src/Controllers/ErrorController.php
@@ -10,21 +10,35 @@ class ErrorController extends AbstractController
 {
     public function index(\Exception $e, Request $request, $code)
     {
-        return $this->render(["code" => $code, "message" => $this->getMessage($code)]);
+        return $this->render([
+            "code" => $code,
+            "message" => $this->getMessage($code),
+            "video" => $this->getVideo($code)
+        ]);
     }
 
     protected function getMessage($code)
     {
         switch ($code) {
             case 404:
-                // This is a bit of a hacky way of doing it, but I think I'll manage to sleep at night
-                return '<iframe width="1080" height="700" src="https://www.youtube.com/embed/84RxK4N1wfE?autoplay=1" frameborder="0" allowfullscreen></iframe>';
+                return "Sorry, I can't help you. I don't know what you're looking for. Although...";
 
             case 500:
                 return "Hmm, probably my bad. I'd say I've logged the error and I'll look into it, but I'd be lying";
 
             default:
                 return "Yup. No clue what you've done. Definitely you though. Not me. My code is perfect.";
+        }
+    }
+
+    protected function getVideo($code)
+    {
+        switch ($code) {
+            case 404:
+                return "https://www.youtube.com/embed/84RxK4N1wfE?autoplay=1";
+
+            default:
+                return false;
         }
     }
 }
